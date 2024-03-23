c93ef149e629782921d948afafaff490bf12edfb
Karina Bulavintseva <karinabulavintseva@gmail.com>
2024-03-23 15:17:01 +0200

    add routes using React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import "./App.css";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Wrapper } from './components/wrapper/wrapper';
+import {
+  Layout,
+  SignInPage,
+  BookPage,
+  CartPage,
+  CatalogPage,
+  NotFoundPage,
+} from './pages';
 
 function App() {
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <Wrapper>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Navigate to='books' />} />
+          <Route path='books' element={<CatalogPage />}></Route>
+          <Route path='books/:bookId' element={<BookPage />}></Route>
+          <Route path='cart' element={<CartPage />} />
+          <Route path='sign-in' element={<SignInPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Wrapper>
   );
 }
 
---
+++ src/pages/catalog-page.tsx
@@ -0,0 +1,5 @@
+const CatalogPage = () => {
+  return <div>catalog page</div>;
+};
+
+export { CatalogPage };
