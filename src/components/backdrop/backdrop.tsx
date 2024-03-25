0585b088068dc64d8ca6460f26bfd552b69506bb
Karina Bulavintseva <karinabulavintseva@gmail.com>
2024-03-25 15:12:52 +0200

    add backdrop component

---
+++ src/components/backdrop/backdrop.tsx
@@ -0,0 +1,18 @@
+import { cn } from '../../utils/cn';
+
+type BackdropProps = {
+  onClick: () => void;
+};
+
+const Backdrop: React.FC<BackdropProps> = ({ onClick }) => {
+  return (
+    <div
+      className={cn(
+        'fixed left-0 top-0 z-10 h-full w-full bg-black/30 transition-[opacity] duration-[200ms] ease-out md:hidden',
+      )}
+      onClick={onClick}
+    ></div>
+  );
+};
+
+export { Backdrop };
