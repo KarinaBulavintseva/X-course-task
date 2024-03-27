248516b1b4aae9717ae75d633fe95ae8723ccf58
Karina Bulavintseva <karinabulavintseva@gmail.com>
2024-03-27 08:07:59 +0200

    add MIN_ITEMS_NUMBER constant

---
+++ src/constants/cart/cart.ts
@@ -0,0 +1,1 @@
+export const MIN_ITEMS_NUMBER = 1;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { FilterOptions } from './filter/filter';
 export { FilterPrices } from './filter/filter';
 export { FilterTitle } from './filter/filter';
+export { MIN_ITEMS_NUMBER } from './cart/cart';
